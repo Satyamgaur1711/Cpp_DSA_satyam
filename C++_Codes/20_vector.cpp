25e0f06aeb1d6fb8d1593943cafa7bb935a5daa7
satyam gaur <satyamji1711@gmail.com>
2026-09-12 22:12:58 +0530

    adding static and dynamic memroy alocation

--- C++_Codes/20_vector.cpp
+++ C++_Codes/20_vector.cpp
@@ -13,8 +13,10 @@ int main()
 {
     vector<int>vec = {10, 20, 30, 40, 50, 60, 70, 80, 90};
     vector<int>ve(4, 3); // 4 size hai and 3 har jagah store hoga.
+    vec.push_back(12121);
+    // vec.pop_back(); // yaha py last wale number delet ho jayegi... jo ke 12121 hai..
 
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 10; i++)
     {
         cout << vec[i] << endl;
     }
@@ -23,7 +25,12 @@ int main()
         cout << i << endl;
     }
     cout << "the size of vector is " << sizeof(vec) << endl; // ye c++ ka fuction hai. isko sirf 3 pointers ke value mil rahi hai Start pointer: Data kahan se shuru ho raha hai.Finish pointer: Data kahan khatam ho raha hai. Capacity pointer: Total memory kahan tak allocate hui hai. 8x3
-
     cout << "The size of vectore is "<< vec.size() << endl; // its inbuild function of STL
+    cout << "the back number is " << vec.back() << endl;
+    cout << "the fornt number is "<< vec.front()<< endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << vec.at(i) << endl;
+    }
     return 0;
 }
---
+++ C++_Codes/21_static_dynamic_memoryallocation.cpp
@@ -0,0 +1,7 @@
+// static memory alloted at compilare run time..
+// dynamic memory are aloted at run time..
+// thats why vectoar are use the are dynamic
+
+// statac memory stack me allot hoti hai 
+// dyanmic memory heap me allot hogti hai 
+
